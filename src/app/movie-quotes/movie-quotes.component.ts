99ce152161eed04d61db3ba9315d2aced4b5f4e2
benneee <benedictiknkeonye@gmail.com>
2019-10-19 15:43:29 +0100

    Created extra necessary components

---
+++ src/app/movie-quotes/movie-quotes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-quotes',
+  templateUrl: './movie-quotes.component.html',
+  styleUrls: ['./movie-quotes.component.scss']
+})
+export class MovieQuotesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
