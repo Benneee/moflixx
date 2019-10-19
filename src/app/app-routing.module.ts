e7222e489bb8d6cb3e216889027b52c2b43e0ad0
benneee <benedictiknkeonye@gmail.com>
2019-10-19 15:42:06 +0100

    Routes configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
+import { MovieQuotesComponent } from './movie-quotes/movie-quotes.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
+import { FavouritesComponent } from './favourites/favourites.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'favourites', component: FavouritesComponent },
+  { path: 'movie-quotes', component: MovieQuotesComponent },
+  { path: 'movie/:movieId', component: MovieDetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
