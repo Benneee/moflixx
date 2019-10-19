431f228b50a98204e16a65e8be19a64a84e06282
benneee <benedictiknkeonye@gmail.com>
2019-10-19 22:08:10 +0100

    Completed homepage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,3 +1,5 @@
+import { Movie } from './../movie/movie.model';
+import { MoviesService } from './../providers/movies.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -5,11 +7,76 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
 
-  constructor() { }
+/**
+ * The items I want
+ * title
+ * year
+ * image
+ * imdbId (for search & details page)
+ */
+export class HomeComponent implements OnInit {
+  constructor(private moviesService: MoviesService) {}
+  isLoading = false;
+  error = false;
+  movies: Movie[];
+  altImg = '../assets/no-img.png';
+  randomMovies;
+  randomNumber;
+  randomMovie;
+  p;
+  previousPage = false;
+  nextPage;
+  totalPageNums;
+
+  moviesUrl = `http://www.omdbapi.com/?apikey=2be45151`;
 
   ngOnInit() {
+    this.randomMovies = ['batman', 'superman', 'avengers'];
+    this.randomNumber = Math.floor(
+      Math.random() * this.randomMovies.length - 1 + 1
+    );
+    this.randomMovie = this.randomMovies[this.randomNumber];
+    this.loadData();
   }
 
+  private loadData(url?: string, pageNumber?: number) {
+    if (!url) {
+      url = this.moviesUrl;
+    }
+    this.moviesService
+      .fetchMovies(url, this.randomMovie, pageNumber)
+      .then(res => {
+        this.isLoading = true;
+        this.error = false;
+        if (res) {
+          this.totalPageNums = res['totalResults'];
+          if (res['Response'] === 'True') {
+            this.movies = res['Search'].map(item => {
+              return {
+                title: item.Title,
+                imdbID: item.imdbID,
+                img: item.Poster ? item.Poster : this.altImg,
+                year: item.Year
+              };
+            });
+            // console.log('movies: ', this.movies);
+          }
+        }
+      })
+      .catch(err => {
+        if (err) {
+          this.isLoading = false;
+          this.error = true;
+          console.log(err);
+        }
+      });
+  }
+
+  getNextPageData() {
+    // console.log(this.totalPageNums);
+    this.totalPageNums = Number(this.totalPageNums);
+    const pageNumber = this.totalPageNums - (this.totalPageNums - 1) + 1;
+    this.loadData(this.moviesUrl, pageNumber);
+  }
 }
