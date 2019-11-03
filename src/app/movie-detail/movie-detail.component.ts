a7ed8c46205ad4ebe0c4e97c22e04b0cdfbcb90f
benneee <benedictiknkeonye@gmail.com>
2019-11-03 17:58:13 +0100

    WIP: Mark movie as favourites from "Movie detail" page

--- src/app/movie-detail/movie-detail.component.ts
+++ src/app/movie-detail/movie-detail.component.ts
@@ -1,3 +1,4 @@
+import { ToastrService } from 'ngx-toastr';
 import { MoviesService } from './../providers/movies.service';
 import { Movie } from './../movie/movie.model';
 import { Component, OnInit } from '@angular/core';
@@ -34,11 +35,14 @@ export class MovieDetailComponent implements OnInit {
   isLoading = false;
   error = false;
   altImg = '../assets/no-img.png';
+  favourites: any;
+  checkArray: any;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private moviesService: MoviesService
+    private moviesService: MoviesService,
+    private toastr: ToastrService
   ) {}
 
   ngOnInit() {
@@ -87,4 +91,35 @@ export class MovieDetailComponent implements OnInit {
       this.movieID = params['movieId'];
     });
   }
+
+  getMoviesFromLS() {
+    let faves;
+    faves =
+      localStorage.getItem('movies') === null
+        ? []
+        : JSON.parse(localStorage.getItem('movies'));
+    return faves;
+  }
+
+  favourite(movie: any) {
+    const title = movie.Title;
+    const storage = localStorage;
+
+    this.favourites = this.getMoviesFromLS();
+    this.checkArray = this.favourites.some((m: any) => m.title === title);
+    if (this.checkArray === false) {
+      this.favourites.push(movie);
+      storage.setItem('movies', JSON.stringify(this.favourites));
+      this.toastr.success(`${title} added to Favourites`, 'Favourites');
+    } else {
+      this.toastr.info(`${title} already in 'Favourites'`, 'Favourites');
+    }
+  }
 }
+/**
+ * To Do
+ * 
+ * Add more of the items being used in Movie Detail to be part of the Movie Model so that 
+ * the movies "favourited" from Movie Detail can show like the other movies there....
+ * It's currently showing as undefined, we can't work with that.
+ */
